9ab0aa24555c1541282312c0474d5a327ba8579d
Alexandru Scrob <Alexandru.Scrob@endava.com>
2026-08-13 18:25:39 +0300

    Added Data Binding app

---
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/Person.java
@@ -0,0 +1,34 @@
+package com.example.databindingapp;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class Person extends BaseObservable {
+    String name, email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Empty constructor for null safety
+    public Person(){}
+}
